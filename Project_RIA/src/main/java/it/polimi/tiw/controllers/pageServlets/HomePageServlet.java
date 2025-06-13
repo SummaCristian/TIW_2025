d705075f19abcb8e82fb131c5978c2066ef10002
Cristian Summa <160849789+SummaCristian@users.noreply.github.com>
2025-06-13 11:40:15 +0200

    Added HomePageServlet
    This Servlet delivers the HomePage HTML file to the User upon requested

---
+++ Project_RIA/src/main/java/it/polimi/tiw/controllers/pageServlets/HomePageServlet.java
@@ -0,0 +1,31 @@
+package it.polimi.tiw.controllers.pageServlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/home")
+public class LoginPageServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        // Retrieve the HTML file
+        String filePath = getServletContext().getRealPath("/WEB-INF/pages/HomePage.html");
+
+        try (var reader = new java.io.BufferedReader(new java.io.FileReader(filePath));
+             var writer = response.getWriter()) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.println(line);
+            }
+        }
+    }
+}
