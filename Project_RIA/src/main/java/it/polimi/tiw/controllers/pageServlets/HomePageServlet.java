b32dd13d99fb559eb12707ed2ad827a8adc99633
Cristian Summa <cristian.summa@mail.polimi.it>
2025-06-14 14:52:23 +0200

    [RIA] Fixed some issues

--- Project_RIA/src/main/java/it/polimi/tiw/controllers/pageServlets/HomePageServlet.java
+++ Project_RIA/src/main/java/it/polimi/tiw/controllers/pageServlets/HomePageServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/home")
-public class LoginPageServlet extends HttpServlet {
+public class HomePageServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
